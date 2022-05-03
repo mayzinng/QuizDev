226412e7d78a84e1e50b2234cd7b25bb307b741f
Maysa <maysaraquel.ia@gmail.com>
2022-05-03 11:24:32 -0300

    Feito função de light mode e dark mode themes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-function mudaTema() {
-    var x = document.getElementById("light");
-    if (x.className === "tema-") {
-        x.className += "dark";
-    } else {
-        x.className = "tema-";
-    }
+// function mudaTema() {
+//     var x = document.getElementById("light");
+//     if (x.className === "tema-") {
+//         x.className += "dark";
+//     } else {
+//         x.className = "tema-";
+//     }
 
+// }
+
+var light = document.getElementById('light');
+
+light.onclick = function() {
+    document.body.classList.toggle("dark-theme");
+}
+
+function quiz() {
+    window.location.href = "choices.html";
 }
