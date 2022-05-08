b007ceda5ca13ae0a6be08eec59c4fc3a6fd925f
Maysa <maysaraquel.ia@gmail.com>
2022-05-07 21:54:43 -0300

    Feito dark mode, será feito função para guardar no localStorage

--- js/script.js
+++ js/script.js
@@ -1,14 +1,59 @@
-var light = document.getElementById('light');
-light.onclick = function() {
-    document.body.classList.toggle("dark-theme");
-}
+// var light = document.getElementById('light');
+// light.onclick = function() {
+//     document.body.classList.toggle("dark-theme");
+// }
 
+//Themes
+
+//window.getComputedStyle(document.getElementById("comecar")).getPropertyValue("color")
 function quiz() {
     window.location.href = "choices.html";
 }
 
-//Animate scroll
+//Themes
 
-var scroll = document.querySelectorAll('#comecar');
 
-console.log(scroll)
+const html = document.querySelector("html");
+const theme = document.querySelector("input[name=theme]");
+
+const getStyle = (element, styles) =>
+    window.getComputedStyle(element).getPropertyValue(styles)
+
+
+const initialColors = {
+    bg: getStyle(html, "--theme-color"),
+    details: getStyle(html, "--details-color"),
+    css: getStyle(html, "--css"),
+    html: getStyle(html, "--html"),
+    js: getStyle(html, "--js"),
+    cardsShadow: getStyle(html, "--shadow-cards"),
+    cardsHover: getStyle(html, "--shadow-hover")
+}
+
+const darkMode = {
+    bg: "#212121",
+    details: "#000",
+    css: "#0C304D",
+    html: "#7E2002",
+    js: "#BF8106",
+    cardsShadow: "rgba(253, 253, 253, 0.192)",
+    cardsHover: "#F5A300"
+}
+
+
+const transformKey = key =>
+    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+
+
+const changeTheme = (colors) => {
+    Object.keys(colors).map(key =>
+        html.style.setProperty(transformKey(key), colors[key])
+    )
+}
+
+
+theme.addEventListener("change", ({ target }) => {
+    target.checked ? changeTheme(darkMode) : changeTheme(initialColors)
+})
+
+//window.getComputedStyle(document.getElementById("comecar")).getPropertyValue("color")
